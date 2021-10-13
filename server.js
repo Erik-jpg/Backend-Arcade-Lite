135ecc2c6909c490f4ab7512c053c2553cdc1ed7
Erik Stone <spologas@gmail.com>
2021-10-12 23:07:51 -0700

    added apollo/client and mongoose, need to add dotenv and then hook up atlas

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 // import React from 'react';
 const { ApolloServer } = require('apollo-server');
-const {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    useQuery,
-    gql
-  } = require("@apollo/client");
+const { connection } = require('./db/connection');
+const resolve = require('./resolvers');
+const typeDefs = require('./typeDefs');
+// const {
+//     ApolloClient,
+//     InMemoryCache,
+//     ApolloProvider,
+//     useQuery,
+//     gql
+//   } = require("@apollo/client");
 // import InMemoryCache from '@apollo/client';
 // import gql from '@apollo/client';  
 // import resolvers from '@apollo/client';
 
-const typeDefs = gql;
+// const typeDefs = gql;
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-const client = new ApolloClient({ 
-    uri: 'http://localhost:4000/',
-    cache: new InMemoryCache(),
-});
+const app = async () => {
+    await connection;
+    server.listen().then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+      });      
+}
+
+// const client = new ApolloClient({ 
+//     uri: 'http://localhost:4000/',
+//     cache: new InMemoryCache(),
+// });
 
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+app();
