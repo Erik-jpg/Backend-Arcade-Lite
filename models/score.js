05199144fbfcc81525ca9d634ddb85ec41718cc6
Erik Stone <spologas@gmail.com>
2021-10-18 19:11:36 -0700

    almost done hooking everything up!

--- models/score.js
+++ models/score.js
@@ -1,16 +1,23 @@
 const { Schema, model } = require('mongoose');
 
 const scoreSchema = new Schema({
-    name: {
+    usernameBody: {
         type: String,
         required: true,
         // no unique and no trim?
     },
-    points: {
+    scorePoints: {
         type: Number,
         required: true,
     }
-});
+},
+{
+    // I think this will help to save everything as a string and then we can parse info from string into username and score?
+    toJSON: {
+        getters: true
+    }
+}
+);
 
 const Score = model('Score', scoreSchema);
 module.exports = Score;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,4 +1,28 @@
 const db = require('../config/connection');
+const  client = new MongoClient('mongodb://localhost:27017');
+const { MongoClient } = require('mongodb');
+
 const { Score } = require('../models');
 
 const scoreData = require('./score.json');
+
+const Score = [
+    {
+        username: '',
+        body: '',
+    },
+];
+
+const seed = async () => {
+    const connection = await client.connect().catch(console.error);
+    const Score = connection.db('userScore').collection('score');
+    await Score.deleteMany({}).then(console.log);
+    await Score.insertMany({}).then(console.log);
+    connection.close();
+}
+seed();
+
+module.exports = { 
+    Score,
+    scoreData
+};
