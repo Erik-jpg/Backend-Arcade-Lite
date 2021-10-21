b166caf839b7b1d8cff84f94ae7ba0249f9d3e47
Kalyn Reid <reidkalyn361@gmail.com>
2021-10-20 19:43:19 -0700

    Removed un-needed npm packages. Added port for server.listen. Added some error handling.

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const { ApolloServer } = require("apollo-server");
-const { typeDefs, resolvers } = require('./schemas');
-const connection = require('./config/connection');
-require('dotenv').config();
-
+const { typeDefs, resolvers } = require("./schemas");
+const connection = require("./config/connection");
+require("dotenv").config();
 
 const app = async () => {
-  await connection;
-  // const db = connection.db("Arcade-lite");
-  console.log("Connected to MongoDB")
+  try {
+    await connection;
+    console.log("Connected to MongoDB");
+  } catch {
+    (err) => console.log(err);
+  }
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // context: { db },
     formatError: (err) => {
       console.error("Error in apollo: ", err);
       return err;
     },
   });
-  server.listen().then(({ url }) => console.log(`Server ready at ${url}`));
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => console.log(`Server ready at ${url}`));
 };
 
 app();
